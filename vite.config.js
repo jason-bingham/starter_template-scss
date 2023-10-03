efd34346b312f92bd60ca3ab7e8d2fc383ec9f26
jbingham223 <jbingham223@gmail.com>
2023-10-03 11:42:03 -0300

    basic template upgrades

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { resolve } from "path";
 import { defineConfig } from "vite";
+import htmlPurge from "vite-plugin-purgecss";
 
 export default defineConfig({
   root: resolve(__dirname, "src/"),
@@ -8,10 +9,11 @@ export default defineConfig({
     emptyOutDir: true,
     rollupOptions: {
       input: {
-        home: resolve(__dirname, "index.html"),
-        about: resolve(__dirname, "about/index.html"),
-        contact: resolve(__dirname, "contact/index.html"),
+        home: resolve(__dirname, "src/index.html"),
+        // about: resolve(__dirname, "about/index.html"),
+        // contact: resolve(__dirname, "contact/index.html"),
       },
     },
   },
+  plugins: [htmlPurge()],
 });
